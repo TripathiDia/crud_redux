f41167f8421d3e4411dbc420c2f4d0b49dd36727
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2023-05-04 11:41:34 +0530

    Item action and model

---
+++ lib/model/cart_item.dart
@@ -0,0 +1,11 @@
+class CartItem {
+  String name;
+  bool checked;
+
+  CartItem({required this.name, required this.checked});
+
+  @override
+  String toString() {
+    return "$name : $checked";
+  }
+}
